709cad0fbf62057824787143a7ef9407f51db996
Edimar Duran <Edimar Duran>
2022-03-21 14:15:59 -0300

    bootcamp

---
+++ 3. Python_web/shows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\edima\Desktop\bootcamp-igti-python\3. Python_web\shows\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
